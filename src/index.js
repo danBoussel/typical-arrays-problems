6918ad4a9eb6e1f5200f73cc277a3c8ceb1cce0a
Daniil Boussel <daniil7142@gmail.com>
2021-03-11 21:14:27 -0500

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array) return 0
+    return array.reduce((acc, el) => {
+        if (acc > el) acc = el
+        return acc
+    }, 0)
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+    if (!array) return 0
+    return array.reduce((acc, el) => {
+        if (acc < el) acc = el
+        return acc
+    }, 0)
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array) return 0
+    return array.reduce((acc, el) => {
+        acc += el / array.length
+        return Number(acc.toFixed(2))
+    }, 0)
 }
